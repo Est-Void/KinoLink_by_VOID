6d80f603329adce6627ac9cf63ba0f28d23be7f1
V01D4GE <tda.dev@proton.me>
2026-02-16 19:47:33 +0300

    started

---
+++ userscript/kinolink.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         KinoLink by VOID <3
+// @namespace    kinolink
+// @version      0.1
+// @description  light player for kinopoisk 
+// @author       V01D4GE
+// @match        *://www.kinopoisk.ru/*
+// @icon         none
+// @grant        none
+// ==/UserScript==
